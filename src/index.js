e12812c89b2542b9e9486d80a7481ecf9e8191a0
Osman Akar <oakar.elk@gmail.com>
2020-01-24 01:01:51 +0300

    denormalize added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 class SimpleNormalizer {
-  normalize(data, property = 'children') {
+  normalize(data, property = "children") {
     const result = {};
 
     if (!data || data.length === 0) {
@@ -14,19 +14,59 @@ class SimpleNormalizer {
     if (Array.isArray(property)) {
       property.forEach(prop => {
         _recursiveNormalize(result, data, prop);
-      })
+      });
     } else {
       _recursiveNormalize(result, data, property);
     }
 
     return result;
   }
+
+  denormalize(data, property = "children") {
+    const visiteds = {};
+
+    const goDeep = (data, eData, property = "children") => {
+      let children = [];
+      let deepChildren = [];
+
+      if (eData.children && eData.children.length > 0) {
+        children = eData.children.map(cData => {
+          deepChildren = goDeep(data, data[cData], property);
+
+          visiteds[data[cData].id] = true;
+
+          return {
+            ...data[cData],
+            [property]: deepChildren
+          };
+        });
+      }
+
+      return children;
+    };
+
+    const unFilteredDenormalizedData = Object.keys(data).map(e => {
+      const eData = data[e];
+      const children = goDeep(data, eData);
+
+      return {
+        ...eData,
+        [property]: children
+      };
+    });
+
+    const denormalizedData = unFilteredDenormalizedData.filter(
+      dData => !visiteds[dData.id]
+    );
+
+    return denormalizedData;
+  }
 }
 
 const _recursiveNormalize = (result, data, prop) => {
   data.forEach(eachData => {
     let children = [];
-    const propertyArr = prop.split('.');
+    const propertyArr = prop.split(".");
     const firstItem = propertyArr[0];
     const lastItemIndex = propertyArr.length - 1;
     const deep = _findDeep(propertyArr, eachData);
@@ -39,17 +79,17 @@ const _recursiveNormalize = (result, data, prop) => {
     result[eachData.id] = {
       ...eachData,
       ...result[eachData.id],
-      [propertyArr[lastItemIndex]]: children,
+      [propertyArr[lastItemIndex]]: children
     };
 
     delete result[eachData.id][firstItem];
   });
-}
+};
 
 const _findDeep = (propertyArr, data) => {
   return propertyArr.reduce((acc, cur) => {
     return acc && acc[cur];
   }, data);
-}
+};
 
 export default new SimpleNormalizer();
